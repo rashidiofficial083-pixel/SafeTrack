e64c50fcbab9a9a070649688ba74438e775bf0d5
rashidiofficial083-pixel <rashidiofficial083@gmail.com>
2026-09-06 14:12:27 +0600

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.safetrack.app',
+  appName: 'SafeTrack',
+  webDir: 'dist',
+};
+
+export default config;
