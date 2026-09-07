a53015fc02dcdd803b08fc164cfc2d6d14114fd4
rashidiofficial083-pixel <rashidiofficial083@gmail.com>
2026-09-07 19:34:44 +0600

    Add files via upload

--- android/app/src/main/java/com/safetrack/app/MainActivity.java
+++ android/app/src/main/java/com/safetrack/app/MainActivity.java
@@ -2,12 +2,10 @@ package com.safetrack.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.googleauth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(GoogleAuth.class);
         super.onCreate(savedInstanceState);
     }
 }
